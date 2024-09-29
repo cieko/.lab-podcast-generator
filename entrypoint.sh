e1d6fc2bb735f97bd2746579d4e0adbf6b59b0df
Cieko <110197023+cieko@users.noreply.github.com>
2024-09-29 15:39:18 +0000

    push flow add with actions

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "================"
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.name "${INPUT_EMAIL}"
+git config --global --add safe.directory /github/workspace
+
+python3 /usr/bin/feed.py
+
+git add -A && git commit -m "Update Feed"
+git push --set-upstream origin main
+
+
+
+echo "================"
